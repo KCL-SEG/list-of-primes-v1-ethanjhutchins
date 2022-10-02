811f2c1ff8b12aaa8d75d439c78dda235f035613
Ethan Hutchins <ethanjameshutchins@gmail.com>
2022-10-02 21:47:29 +0100

    Add primes functionality.

--- primes.py
+++ primes.py
@@ -3,4 +3,21 @@
 
 def primes(number_of_primes):
     list = []
+    n = 2
+    if number_of_primes == 1:
+        list.extend([n])
+    else:
+        list.extend([n])
+        n = n + 1
+        counter = 0
+        while len(list) < number_of_primes:
+            counter = 0
+            for i in range(len(list)):
+                if n%list[i]!=0:
+                    counter = counter + 1
+            if counter == len(list):
+                list.extend([n])
+                n = n + 2
+            else:
+                n = n + 2
     return list
